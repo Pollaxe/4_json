c33cef6ae3e7e1505556b4abb61426ac2f7de27c
Pollaxe <bobrax@yandex.ru>
2018-01-15 17:46:58 +0300

    настроил нужные функции

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import sys
+import codecs
 
 
 def load_data(filepath):
-    pass
+    data = codecs.open(filepath, encoding='utf-8').read()
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    data = json.loads(data)
+    print(json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    data = load_data(filepath)
+    pretty_print_json(data)
