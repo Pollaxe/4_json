0ef7f36e2b7960346f3aaf50b7b5808e2b09e068
Pollaxe <bobrax@yandex.ru>
2018-01-16 10:39:30 +0300

    переименованы переменные

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,17 @@ import sys
 import codecs
 
 
-def load_data(filepath):
-    data = codecs.open(filepath, encoding='utf-8').read()
-    return data
+def load_data(file_path):
+    json_content = codecs.open(file_path, encoding='utf-8').read()
+    return json_content
 
 
-def pretty_print_json(data):
-    data = json.loads(data)
-    print(json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+def pretty_print_json(json_content):
+    json_content = json.loads(json_content)
+    print(json.dumps(json_content, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    filepath = sys.argv[1]
-    data = load_data(filepath)
-    pretty_print_json(data)
+    file_path = sys.argv[1]
+    json_content = load_data(file_path)
+    pretty_print_json(json_content)
