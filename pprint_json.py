87fb95a76aed1a68a28f9f29f690b2d196e632dc
Pollaxe <bobrax@yandex.ru>
2018-01-17 15:13:29 +0300

    добавлены исключения и переименована переменная

--- pprint_json.py
+++ pprint_json.py
@@ -9,11 +9,22 @@ def load_data(file_path):
 
 
 def pretty_print_json(json_content):
-    json_content = json.loads(json_content)
-    print(json.dumps(json_content, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+    json_decoded = json.loads(json_content)
+    print(json.dumps(json_decoded, sort_keys=True, ensure_ascii=False,
+                     indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    json_content = load_data(file_path)
-    pretty_print_json(json_content)
+    try:
+        file_path = sys.argv[1]
+        json_content = load_data(file_path)
+        pretty_print_json(json_content)
+    except IndexError:
+        try:
+            file_path = input('Please input filename:')
+            json_content = load_data(file_path)
+            pretty_print_json(json_content)
+        except FileNotFoundError:
+            print('File not found, please try again.')
+    except FileNotFoundError:
+        print('File not found, please try again.')
